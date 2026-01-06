e0562e7968b08156f0e2294c267feff9d258b85f
Vasunai <108564021+Vasunai@users.noreply.github.com>
2026-01-06 06:49:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,1 @@
-const elements = document.querySelectorAll('.fade');
 
-const observer = new IntersectionObserver(
-  (entries) => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add('show');
-      } else {
-        entry.target.classList.remove('show');
-      }
-    });
-  },
-  {
-    threshold: 0.15
-  }
-);
-
-elements.forEach(el => observer.observe(el));
