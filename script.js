329d8a5cc683923c0b4615757a4055bd47df31f3
Vasunai <108564021+Vasunai@users.noreply.github.com>
2026-01-06 06:40:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-const fades = document.querySelectorAll('.fade');
-
-const observer = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add('show');
-    }
-  });
-});
-
-fades.forEach(el => observer.observe(el));
+// reserved for future animations
